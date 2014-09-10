2c1a1b819abc43734fe977b443798470bca3a26d
novero-a <novero.a@husky.neu.edu>
2014-09-09 20:37:08 -0700

    CS 2500: Assignment0: Lab 1
    GitHub Contact and email

---
+++ Assignment0.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Assignment0) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;; CS2500 
+;; Assignemnt 0 
+;; Aileen Novero (novero.a@husky.neu.edu)
